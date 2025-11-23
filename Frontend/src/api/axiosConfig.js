709b0970bbac9cc9c1e8f49a8f1518c7c61acd5d
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-11-23 15:39:14 +0530

    first commit

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Update with your live URL when deploying!
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://skillwise-inventory.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
